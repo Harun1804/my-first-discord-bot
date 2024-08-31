64304d43bb0f1d1d108fa7fcb662cef7e7cd50ab
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-08-31 08:49:34 +0700

    First interaction

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
 })
 
 const TOKEN = process.env.BOT_TOKEN
 
 client.login(TOKEN)
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} Has online`)
+})
+
+client.on('messageCreate', (message) => {
+  if (message.content == 'ping') {
+    console.log(message.author.tag);
+    message.reply('pong')
+  }
+})
