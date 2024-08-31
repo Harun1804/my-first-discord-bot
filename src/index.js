87bd19c7767aec7d7821bcff0c959e55bcb88939
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-08-31 09:08:55 +0700

    Command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { Client, GatewayIntentBits } from 'discord.js'
+import { Client, GatewayIntentBits, Routes } from 'discord.js'
 import dotenv from 'dotenv'
+import { REST } from '@discordjs/rest'
 
 dotenv.config()
+
+const TOKEN = process.env.BOT_TOKEN
+const CLIENT_ID = process.env.BOT_CLIENT_ID
+const GUILD_ID = process.env.BOT_GUILD_ID
 
 const client = new Client({
   intents: [
@@ -11,17 +16,36 @@ const client = new Client({
   ]
 })
 
-const TOKEN = process.env.BOT_TOKEN
+const rest = new REST({ version: '10' }).setToken(TOKEN)
 
-client.login(TOKEN)
 
-client.on('ready', () => {
-  console.log(`${client.user.tag} Has online`)
-})
+client.on('ready', () => console.log(`${client.user.tag} Has online`))
 
-client.on('messageCreate', (message) => {
-  if (message.content == 'ping') {
-    console.log(message.author.tag);
-    message.reply('pong')
+async function main() {
+  const commands = [
+    {
+      name: 'ping',
+      description: 'Replies with Pong!',
+    },
+    {
+      name: 'server',
+      description: 'Replies with server info!',
+    }
+  ]
+
+  try {
+    console.log('Started refreshing application (/) commands.')
+    
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    )
+    
+    console.log('Successfully reloaded application (/) commands.')
+    client.login(TOKEN)
+  } catch (error) {
+    console.error(error)
   }
-})
+}
+
+main()
