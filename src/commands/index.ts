f954c643df1b341ac2cddbb1920eae88292491d8
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2026-08-13 21:22:05 +0700

    rem: remove about command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,9 @@
-import about from './utility/about.js';
 import ping from './utility/ping.js';
 import shortener from './utility/shortener.js';
 import help from './utility/help.js';
 import type { Command } from '@/types/command.js';
 
-const commandModules: Command[] = [about, ping, shortener, help];
+const commandModules: Command[] = [ping, shortener, help];
 
 export const commands = new Map(commandModules.map((command) => [command.data.name, command]));
 export const commandData = commandModules.map((command) => command.data.toJSON());
